0061539a812949978e3bce86fb44c6a00771952c
Mohan Zhang <mohanzhang@gmail.com>
2011-04-15 23:47:23 -0500

    New rails project (3.0.6)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agrias::Application.config.secret_token = '022627a6e3f0b5ec418a3da5e4315c18cdfc4ad15a4e92f14cfa9814b3c78deff03fd2e7c1dc31b3546cc10021de39801ac8169758a699751bd94eb6d3c7d454'
