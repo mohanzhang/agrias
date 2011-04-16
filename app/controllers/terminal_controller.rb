e8c2dd818f2da7966af4cf4335dbfe8494c08b43
Mohan Zhang <mohanzhang@gmail.com>
2011-04-16 17:32:29 -0500

    Scaffolding for echo command

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -1,2 +1,13 @@
 class TerminalController < ApplicationController
+  before_filter :set_args
+
+  def echo
+    
+  end
+
+  private
+
+  def set_args
+    @args = params[:args]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Agrias::Application.routes.draw do
   root :to => "terminal#index"
+
+  match "echo", :to => "terminal#echo"
 end
