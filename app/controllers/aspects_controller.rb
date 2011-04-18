905b6eeb6544c48a481b5de57c601bc96b35267f
Mohan Zhang <mohanzhang@gmail.com>
2011-04-18 18:59:42 -0500

    REST in place for aspects

--- app/controllers/aspects_controller.rb
+++ app/controllers/aspects_controller.rb
@@ -1,9 +1,23 @@
 class AspectsController < InheritedResources::Base
   before_filter :pass_args, :only => [:create]
 
+  respond_to :html, :json
+
   layout false
 
   def index
     @aspects = Aspect.roots
+  end
+
+  def show
+    show! do |format|
+      format.json { render :json => @aspect }
+    end
+  end
+
+  def update
+    update! do |format|
+      format.json { render :nothing => true }
+    end
   end
 end
--- app/helpers/aspects_helper.rb
+++ app/helpers/aspects_helper.rb
@@ -1,7 +1,7 @@
 module AspectsHelper
   def aspect_tree(node)
     children = node.children.order("weight DESC").map {|c| aspect_tree(c)}.join("\n")
-    node_html = node.name
+    node_html = rip_element(node, :name)
 
     return "<ul class=\"aspectTree\"><li>#{node_html}#{children}</li></ul>"
   end
