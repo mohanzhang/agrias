31946da2466d761a683ae4349cc01cb24e5f82f7
Mohan Zhang <mohanzhang@gmail.com>
2011-04-18 19:27:42 -0500

    REST in place for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,3 +1,17 @@
 class AppointmentsController < InheritedResources::Base
   before_filter :pass_args, :only => :create
+
+  respond_to :html, :json
+
+  def show
+    show! do |format|
+      format.json { render :json => @appointment }
+    end
+  end
+
+  def update
+    update! do |format|
+      format.json { render :nothing => true }
+    end
+  end
 end
