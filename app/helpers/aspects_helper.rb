a857c6b5fa85f8b84eaa6f48a7b52716e8cfda16
Mohan Zhang <mohanzhang@gmail.com>
2011-04-19 17:40:15 -0500

    Apply extension UI pattern to aspects and appointments

--- app/helpers/aspects_helper.rb
+++ app/helpers/aspects_helper.rb
@@ -1,7 +1,7 @@
 module AspectsHelper
   def aspect_tree(node)
     children = node.children.order("weight DESC").map {|c| aspect_tree(c)}.join("\n")
-    node_html = rip_element(node, :name)
+    node_html = link_to node.name, aspect_path(node), :remote => true, "data-output-mode" => "extend"
 
     return "<ul class=\"aspectTree\"><li>#{node_html}#{children}</li></ul>"
   end
