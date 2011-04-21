28b7706d8622bbbf5d74ba646f2446e954c8f3de
Mohan Zhang <mohanzhang@gmail.com>
2011-04-20 22:01:18 -0500

    Aesthetic improvements to show and index actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,5 @@
 class TasksController < InheritedResources::Base
   before_filter :pass_args, :only => [:create, :update]
+
+  layout false
 end
---
+++ config/initializers/datetime_formats.rb
@@ -0,0 +1,2 @@
+Date::DATE_FORMATS[:default] = "%B %d, %Y"
+Time::DATE_FORMATS[:default] = "#{Date::DATE_FORMATS[:default]} at %I:%M%p"
