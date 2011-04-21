5e9b4a9c84e19c8910c0780c35e1bd9e96cc8d43
Mohan Zhang <mohanzhang@gmail.com>
2011-04-20 22:32:26 -0500

    Can now edit and delete tasks (but not change the due date)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < InheritedResources::Base
   before_filter :pass_args, :only => [:create, :update]
 
   layout false
+
+  def show
+    show! do |format|
+      format.json { render :json => @task }
+    end
+  end
+
+  def update
+    update! do |format|
+      format.json { render :nothing => true }
+    end
+  end
 end
