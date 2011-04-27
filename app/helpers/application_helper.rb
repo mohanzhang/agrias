cc961e91544980393f721a543c121abaf80d335b
Mohan Zhang <mohanzhang@gmail.com>
2011-04-26 19:40:11 -0500

    Add primitive version of notes to tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
-  def rip_element(model, attribute)
+  def rip_element(model, attribute, formtype="input")
+    text = "Click to edit"
+    
+    unless model.send(attribute).blank?
+      text = model.send(attribute)
+      text = simple_format(text) if formtype == "textarea"
+    end
+
     %{<span class="rest_in_place"
         data-url="#{polymorphic_path(model)}" data-object="#{model.class.to_s.underscore}"
-        data-attribute="#{attribute}">#{model.send(attribute)}</span>}
+        data-attribute="#{attribute}" data-formtype="#{formtype}">#{text}</span>}
   end
 
   def delete_link(model)
